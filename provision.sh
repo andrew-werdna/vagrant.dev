939e095d5812a3b48403f232893bf3138110a934
Esbjörn Eriksson <esbjorn@bazooka.se>
2014-05-19 16:49:52 +0200

    Do ssh key generation and install on separate steps.

--- provision.sh
+++ provision.sh
@@ -17,6 +17,9 @@ fi
 
 # Create private key for virtual machine and allow it to connect to iself, also add host to known hosts
 # Note: This script is run as root, but ansible then uses the vagrant user when connecting
-if [[ ! -f /provision-ssh-run ]]; then 
-	ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa && cat ~/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys && ssh-keyscan -H 127.0.0.1 >> ~/.ssh/known_hosts && touch /provision-ssh-run
+if [[ ! -f ~/.ssh/id_rsa ]]; then 
+	ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+fi
+if [[ ! -f /provision-ssh-run ]]; then
+	cat ~/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys && ssh-keyscan -H 127.0.0.1 >> ~/.ssh/known_hosts && touch /provision-ssh-run
 fi
